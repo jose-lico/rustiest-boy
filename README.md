6666fa7c54bdf7be4d68035988dc60ce66e20d43
jose-lico <zelico98@gmail.com>
2023-04-22 17:40:21 +0100

    Added sdl2-ttf dependency and boilerplate

--- README.md
+++ README.md
@@ -1,18 +1,27 @@
 # rustiest-boy
-Another GameBoy emulator built in Rust
+
+Yet another GameBoy emulator built in Rust
 
 ## Goals
 
 To build a cross platform GameBoy Color emulator in Rust.
 
  - Support for desktop platforms and .wasm
- - Support for most gameboy color cartridge types and features
- - Farfetched: Support for link cable emulation and save data
+ - Support for most gameboy color cartridge types and basic features
+ - [Farfetch'd](https://assets.pokemon.com/assets/cms2/img/pokedex/full/083.png): Support for more advanced features such as: link cable emulation, save data & audio
 
-I'm building this with the intention of learning a bit about rust as well as the whole .wasm thing.
+I'm building this with the intention of learning about rust, the whole .wasm thing & (gameboy) emulation.
 
 ## Building
 
 ### Linux
 
  - Install sdl2 `sudo apt install libsdl2-dev`
+ - Install sdl2-ttf `sudo apt-get install libsdl2-ttf-dev`
+
+ Run `cargo run`
+
+ <p align="center">
+<img src=".github/images/rustiest-boy-linux.png">
+<i>Amazing demo window with text</i>
+</p>
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,22 @@ extern crate sdl2;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::TextureQuery;
+use sdl2::ttf;
+
 use std::time::Duration;
+
+static SCREEN_WIDTH: u32 = 500;
+static SCREEN_HEIGHT: u32 = 450;
 
 pub fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
+    let ttf_context = ttf::init().map_err(|e| e.to_string())?;
     let video_subsystem = sdl_context.video()?;
 
     let window = video_subsystem
-        .window("rustiest-boy", 500, 450)
+        .window("rustiest-boy", SCREEN_WIDTH, SCREEN_HEIGHT)
         .position_centered()
         .opengl()
         .build()
@@ -18,9 +26,25 @@ pub fn main() -> Result<(), String> {
 
     let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
 
-    canvas.set_draw_color(Color::RGB(209,13,115));
-    canvas.clear();
-    canvas.present();
+    canvas.set_draw_color(Color::RGB(209, 13, 115));
+
+    let texture_creator = canvas.texture_creator();
+
+    let font = ttf_context.load_font("res/PokemonGbFont.ttf", 20)?;
+
+    let text = font
+        .render("Hello World")
+        .blended(Color::RGBA(255, 255, 255, 255))
+        .map_err(|e| e.to_string())?;
+
+    let texture = texture_creator
+        .create_texture_from_surface(&text)
+        .map_err(|e| e.to_string())?;
+
+    let TextureQuery { width, height, .. } = texture.query();
+
+    let target = Rect::new(0, 0, width.try_into().unwrap(), height.try_into().unwrap());
+
     let mut event_pump = sdl_context.event_pump()?;
 
     'running: loop {
@@ -36,9 +60,12 @@ pub fn main() -> Result<(), String> {
         }
 
         canvas.clear();
+
+        canvas.copy(&texture, None, Some(target))?;
+        
         canvas.present();
+
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
-        // The rest of the game loop goes here...
     }
 
     Ok(())
