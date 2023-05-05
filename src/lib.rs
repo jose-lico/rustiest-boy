01ab38df51547faf568502a55cbd42ac7046abff
jose-lico <zelico98@gmail.com>
2023-05-05 19:43:54 +0100

    Added egui demo window

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+use std::{time::Instant, iter};
+
+use egui_wgpu_backend::{ScreenDescriptor, RenderPass};
 use winit::{
-    event::*,
     event_loop::{ControlFlow, EventLoop},
 };
 
@@ -9,6 +11,22 @@ use wasm_bindgen::prelude::*;
 mod utils;
 
 use utils::window::Window;
+
+/// A custom event type for the winit app.
+enum Event {
+    RequestRedraw,
+}
+
+/// This is the repaint signal type that egui needs for requesting a repaint from another thread.
+/// It sends the custom RequestRedraw event to the winit event loop.
+struct ExampleRepaintSignal(std::sync::Mutex<winit::event_loop::EventLoopProxy<Event>>);
+
+impl epi::backend::RepaintSignal for ExampleRepaintSignal {
+    fn request_repaint(&self) {
+        self.0.lock().unwrap().send_event(Event::RequestRedraw).ok();
+    }
+}
+
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub fn run() {
@@ -23,25 +41,112 @@ pub fn run() {
 
     let event_loop = EventLoop::new();
 
-    let window = Window::new(500, 450, &event_loop);
+    let mut window = Window::new(500, 450, &event_loop);
 
-    event_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent {
-            ref event,
-            window_id,
-        } if window_id == window.handle.id() => match event {
-            WindowEvent::CloseRequested
-            | WindowEvent::KeyboardInput {
-                input:
-                    KeyboardInput {
-                        state: ElementState::Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::Escape),
-                        ..
-                    },
-                ..
-            } => *control_flow = ControlFlow::Exit,
+    let mut egui_rpass = RenderPass::new(&window.device, wgpu::TextureFormat::Bgra8Unorm , 1);
+
+    let mut demo_app = egui_demo_lib::DemoWindows::default();
+
+    let start_time = Instant::now();
+
+    event_loop.run(move |event, _, control_flow| {
+        window.platform.handle_event(&event);
+
+        match event {
+            winit::event::Event::RedrawRequested(..) => {
+                window
+                    .platform
+                    .update_time(start_time.elapsed().as_secs_f64());
+
+                let output_frame = match window.surface.get_current_texture() {
+                    Ok(frame) => frame,
+                    Err(wgpu::SurfaceError::Outdated) => {
+                        // This error occurs when the app is minimized on Windows.
+                        // Silently return here to prevent spamming the console with:
+                        // "The underlying surface has changed, and therefore the swap chain must be updated"
+                        return;
+                    }
+                    Err(e) => {
+                        eprintln!("Dropped frame with error: {}", e);
+                        return;
+                    }
+                };
+
+                let output_view = output_frame
+                    .texture
+                    .create_view(&wgpu::TextureViewDescriptor::default());
+
+                window.platform.begin_frame();
+
+                // Draw the demo application.
+                demo_app.ui(&window.platform.context());
+
+                // End the UI frame. We could now handle the output and draw the UI with the backend.
+                let full_output = window.platform.end_frame(Some(&window.handle));
+                let paint_jobs = window.platform.context().tessellate(full_output.shapes);
+
+                let mut encoder =
+                    window
+                        .device
+                        .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                            label: Some("encoder"),
+                        });
+
+                // Upload all resources for the GPU.
+                let screen_descriptor = ScreenDescriptor {
+                    physical_width: window.surface_config.width,
+                    physical_height: window.surface_config.height,
+                    scale_factor: window.handle.scale_factor() as f32,
+                };
+                let tdelta: egui::TexturesDelta = full_output.textures_delta;
+
+                egui_rpass
+                    .add_textures(&window.device, &window.queue, &tdelta)
+                    .expect("add texture ok");
+                egui_rpass.update_buffers(&window.device, &window.queue, &paint_jobs, &screen_descriptor);
+
+                // Record all render passes.
+                egui_rpass
+                    .execute(
+                        &mut encoder,
+                        &output_view,
+                        &paint_jobs,
+                        &screen_descriptor,
+                        Some(wgpu::Color::BLACK),
+                    )
+                    .unwrap();
+                // Submit the commands.
+                window.queue.submit(iter::once(encoder.finish()));
+
+                // Redraw egui
+                output_frame.present();
+
+                egui_rpass
+                    .remove_textures(tdelta)
+                    .expect("remove texture ok");
+            }
+
+            winit::event::Event::MainEventsCleared | winit::event::Event::UserEvent(()) => {
+                window.handle.request_redraw();
+            }
+
+            winit::event::Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.handle.id() => match event {
+                winit::event::WindowEvent::CloseRequested
+                | winit::event::WindowEvent::KeyboardInput {
+                    input:
+                        winit::event::KeyboardInput {
+                            state: winit::event::ElementState::Pressed,
+                            virtual_keycode: Some(winit::event::VirtualKeyCode::Escape),
+                            ..
+                        },
+                    ..
+                } => *control_flow = ControlFlow::Exit,
+                _ => {}
+            },
             _ => {}
-        },
-        _ => {}
+        }
     });
 }
--- src/utils/window.rs
+++ src/utils/window.rs
@@ -1,10 +1,18 @@
 use std::process::exit;
 
-use log::trace;
+use egui::FontDefinitions;
+use egui_winit_platform::{Platform, PlatformDescriptor};
+use log::{error, trace};
+use wgpu::{Surface, Device, SurfaceConfiguration, Queue};
 use winit::{dpi::LogicalSize, event_loop::EventLoop, window};
 
 pub struct Window {
     pub handle: window::Window,
+    pub platform : Platform,
+    pub surface : Surface,
+    pub device: Device,
+    pub surface_config : SurfaceConfiguration,
+    pub queue : Queue
 }
 
 impl Window {
@@ -20,10 +28,56 @@ impl Window {
         let window = match window_result {
             Ok(window) => window,
             Err(error) => {
-                eprintln!("Failed to create window: {}", error);
+                error!("Failed to create window: {}", error);
                 exit(1);
             }
         };
+
+        let descriptor = wgpu::InstanceDescriptor::default();
+        let instance = wgpu::Instance::new(descriptor);
+        let surface = unsafe { instance.create_surface(&window) }.unwrap();
+
+        // WGPU 0.11+ support force fallback (if HW implementation not supported), set it to true or false (optional).
+        let adapter = pollster::block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::HighPerformance,
+            compatible_surface: Some(&surface),
+            force_fallback_adapter: false,
+        }))
+        .unwrap();
+
+        let (device, queue) = pollster::block_on(adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                features: wgpu::Features::default(),
+                limits: wgpu::Limits::default(),
+                label: None,
+            },
+            None,
+        ))
+        .unwrap();
+
+        let size = window.inner_size();
+        let surface_format = surface.get_capabilities(&adapter).formats;
+        //let surface_format = surface.get_current_texture().unwrap().texture.format();
+
+        let surface_config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: wgpu::TextureFormat::Bgra8Unorm,
+            width: size.width as u32,
+            height: size.height as u32,
+            present_mode: wgpu::PresentMode::Fifo,
+            alpha_mode: wgpu::CompositeAlphaMode::Auto,
+            view_formats: surface_format,
+        };
+
+        surface.configure(&device, &surface_config);
+
+        let platform = Platform::new(PlatformDescriptor {
+            physical_width: size.width as u32,
+            physical_height: size.height as u32,
+            scale_factor: window.scale_factor(),
+            font_definitions: FontDefinitions::default(),
+            style: Default::default(),
+        });
 
         #[cfg(target_arch = "wasm32")]
         {
@@ -39,6 +93,6 @@ impl Window {
                 .expect("Couldn't append canvas to document body.");
         }
 
-        Window { handle: window }
+        Window { handle: window, platform, surface, device, surface_config, queue }
     }
 }
